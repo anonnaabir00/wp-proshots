5cfba1a47ebbf81f0153210306afd0a56343f84f
anonnaabir00 <anonnaabir00@gmail.com>
2020-10-12 20:15:13 +0600

    Author URI Updated

--- proshots.php
+++ proshots.php
@@ -12,7 +12,6 @@
      * Description:       Sell your photos, arts, vectors using Woocommerce. Included with Custom Product Type, Watermark Support and much more.
      * Version:           1.0
      * Author:            Codember
-     * Author URI:        https://codember.com/
      * License:           GPL-2.0+
      * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
      * Text Domain:       wp-proshots
