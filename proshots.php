f4efded1376804e36208b6371f2dd3d79eb4f3f9
anonnaabir00 <anonnaabir00@gmail.com>
2020-10-12 19:10:06 +0600

    Updated

--- proshots.php
+++ proshots.php
@@ -1,26 +1,26 @@
 <?php
 
-/**
- *
- * @link              http://codember.com/
- * @since             1.0
- * @package           Sell your photos, arts, vectors using Woocommerce.
- *
- * @wordpress-plugin
- * Plugin Name:       ProShots Lite For WooCommerce
- * Plugin URI:        http://codember.com/
- * Description:       Sell your photos, arts, vectors using Woocommerce.
- * Version:           1.0
- * Author:            Codember
- * Author URI:        http://codember.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       wp-proshots
+    /**
+     *
+     * @link              https://codember.com/
+     * @since             1.0
+     * @package           Sell your photos, arts, vectors using Woocommerce. Included with Custom Product Type, Watermark Support and much more.
+     *
+     * @wordpress-plugin
+     * Plugin Name:       ProShots For WooCommerce
+     * Plugin URI:        https://codember.com/
+     * Description:       Sell your photos, arts, vectors using Woocommerce. Included with Custom Product Type, Watermark Support and much more.
+     * Version:           1.0
+     * Author:            Codember
+     * Author URI:        https://codember.com/
+     * License:           GPL-2.0+
+     * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+     * Text Domain:       wp-proshots
+     * Tested up to:      5.5
+     * WC requires at least: 2.2
+     * WC tested up to: 4.5.2
 
-* WC requires at least: 2.2
-* WC tested up to: 4.5.2
-
- */
+    */
 
 
 require_once ('functions.php');
