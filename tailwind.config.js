be7c9f44edb30a28f8adf156b0acac98ec7480cf
Asaduzzaman Abir <anonnaabir00@gmail.com>
2024-04-14 12:55:51 +0600

    Added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './ar_src/**/*.ts',
+    './ar_src/**/*.vue',
+    './templates/**/*.html',
+    './parts/**/*.html',
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
