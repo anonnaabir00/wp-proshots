006be0e0f58f8b37b4e334ad7dcabb3c0c31f28e
Asaduzzaman Abir <anonnaabir00@gmail.com>
2024-04-15 21:00:10 +0600

    Updated Shortcode

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -13,6 +13,7 @@ class Shortcode {
     public static function init() {
         $self = new self();
         add_shortcode('proshots_product_grid', array( $self, 'product_grid_shortcode' ));
+        add_shortcode('proshots_product_carousel', array( $self, 'product_carousel_shortcode' ));
     }
 
     // public function product_grid_shortcode() {
@@ -32,6 +33,26 @@ class Shortcode {
 
         $products = Helper::get_products($atts['products']);
         require_once PROSHOT_INCLUDES_DIR_PATH . 'templates/product-card.php';
+        return ob_get_clean();
+    }
+
+    public function product_carousel_shortcode($atts) {
+        ob_start();
+        $atts = shortcode_atts(
+            array(
+                'products' => -1,
+                'product_grid' => 4
+            ),
+            $atts
+        );
+
+        $products = Helper::get_products($atts['products']);
+
+        $template_atts = array(
+            'product_grid' => $atts['product_grid'],
+        );
+
+        require_once PROSHOT_INCLUDES_DIR_PATH . 'templates/product-carousel.php';
         return ob_get_clean();
     }
     
