eea561878b58861405adbaeb97586238da9cbca0
Asaduzzaman Abir <anonnaabir00@gmail.com>
2024-04-15 20:58:47 +0600

    Updated

--- ar_src/main.js
+++ ar_src/main.js
@@ -1,10 +1,36 @@
 // Library
 import { createApp } from 'vue';
-import PrimeVue from 'primevue/config';
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 // Components
-import App from './App.vue'
+// import App from './App.vue'
 
-const app = createApp(App);
-app.mount('#proshost-products');
+// const app = createApp(App);
+// app.mount('#proshost-products');
+
+
+const swiperElement = document.querySelector('.swiper');
+const productGrid = swiperElement.dataset.productgrid;
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: Number(productGrid),
+    spaceBetween: 20,
+    autoplay: {
+        delay: 3000,
+    },
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
