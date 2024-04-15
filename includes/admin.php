efdc02bd0955a7faeb0b2dc2a0ba281b2b4bd758
Asaduzzaman Abir <anonnaabir00@gmail.com>
2024-04-15 20:58:57 +0600

    Updated

--- includes/admin.php
+++ includes/admin.php
@@ -8,18 +8,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Admin {
 
-    public function __construct() {
-        add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
-    }
-
     public static function init() {
         $self = new self();
+        add_action( 'admin_menu', array( $self, 'add_admin_menu' ) );
+        add_action( 'admin_enqueue_scripts', array( $self, 'proshot_admin_scripts' ) );
     }
 
     public function add_admin_menu(){
         add_menu_page( 
-            __( 'WP Proshots', 'wp-proshots' ),
-            'WP Proshots',
+            __( 'Product Showcase For WooCommerce', 'wp-proshots' ),
+            'Product Showcase For WooCommerce',
             'manage_options',
             'wp-proshots-options',
             array($this,'proshots_callback'),
@@ -31,5 +29,16 @@ class Admin {
         ?>
         <div id="proshots-admin"></div>
         <?php
+    }
+
+    public function proshot_admin_scripts(){
+        $current_screen = get_current_screen();
+        $proshot_screen = 'toplevel_page_wp-proshots-options';
+
+        if ($proshot_screen === $current_screen->base) {
+            wp_enqueue_style('proshots-admin-style', PROSHOT_ROOT_DIR_URL . 'includes/assets/style.css');
+            wp_enqueue_style('proshots-admin-main', PROSHOT_ROOT_DIR_URL . 'includes/assets/admin.css');
+            wp_enqueue_script( 'proshots-admin-script', PROSHOT_ROOT_DIR_URL . 'includes/assets/admin.js',[], true, '1.0' );
+        }
     }
 }
